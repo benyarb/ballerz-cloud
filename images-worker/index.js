8bfc7a8971acb65b6c58f969877e718b41d6afd1
Ben Yarbrough <benyarb@gmail.com>
2022-10-23 00:53:25 -0400

    Add deletion script, change ballerz image ids to ballerz/id

--- images-worker/index.js
+++ images-worker/index.js
@@ -1,6 +1,6 @@
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
+addEventListener("fetch", (event) => {
+  event.respondWith(handleRequest(event.request));
+});
 /**
  * Route the image request either to the original image on S3
  * or to Cloudflare Images, depending on the value of "?use_cf_images" in the URL
@@ -11,7 +11,7 @@ const config = {
   // This is the Images account Hash you'll find in your Cloudflare Images Dashboard
   // It is safe to disclose publicly, as this is not a secret value.
   cloudflare_images_account_hash: "HfLkp4Rv9xEt4shRjwi1Gw",
-}
+};
 
 async function handleRequest(request) {
   const url = new URL(request.url);
@@ -24,20 +24,28 @@ async function handleRequest(request) {
   // Use Cloudflare Images to deliver image ✨
   // by cosntructing the CF Images URL with parts extracted from
   // the original website image URLs that this worker intercepts
-  return fetch("https://imagedelivery.net/" + config.cloudflare_images_account_hash + "/" + imageName + "/" + variant, {
-    // relay request headers to Cloudflare Images,
-    // to inform about the media types accepted by the HTTP client
-    headers: request.headers,
-  });
+  return fetch(
+    "https://imagedelivery.net/" +
+      config.cloudflare_images_account_hash +
+      "/" +
+      imageName +
+      "/" +
+      variant,
+    {
+      // relay request headers to Cloudflare Images,
+      // to inform about the media types accepted by the HTTP client
+      headers: request.headers,
+    }
+  );
 }
 
 function extractVariant(url) {
-  // takes website URLs like /image/baller/7562
-  // and identifies the variant (here, "baller")
-  // and imageName ( here, "7562")
-  const parts = url.pathname.replace("/image/", "").split("/");
-  const variant = parts.shift();
-  return { variant: variant, imageName: parts.join("/") };
+  // takes website URLs like /images/ballerz/7562/public
+  // and identifies the variant (here, "public")
+  // and imageName ( here, "ballerz/7562")
+  const parts = url.pathname.replace("/images/", "").split("/");
+  const imageName = parts.splice(0, 2).join("/");
+  return { variant: parts.join("/"), imageName: imageName };
 }
 
 function notFound() {
--- upload.sh
+++ upload.sh
@@ -3,7 +3,7 @@ FILES="/Users/benyarb/Code/NFTG/Ballerz/ballerz-cloud/transparent/*"
 for f in $FILES
 do
   filename=$(basename -- "$f")
-  filename="${filename%.*}"
+  filename="ballerz/${filename%.*}"
   echo "Processing file: $f id: $filename ..."
   curl -X POST -F file=@$f -H "Authorization: Bearer $IMAGE_API_KEY" https://api.cloudflare.com/client/v4/accounts/$IMAGE_ACCOUNT_ID/images/v1 --form "id=$filename"
 done
